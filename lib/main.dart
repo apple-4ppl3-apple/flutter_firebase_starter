2856f8d8cd33d62c79db5b217fb993068220a434
First Last <telecom_06_catnap@icloud.com>
2021-09-21 14:26:29 -0400

    Web Working; iOS still building

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
+
+/// We are using a StatefulWidget such that we only create the [Future] once,
+/// no matter how many times our widget rebuild.
+/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
+/// would re-initialize FlutterFire and make our application re-enter loading state,
+/// which is undesired.
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return const Text("Something Went Wrong");
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return const Text("Loading");
+      },
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -35,6 +78,13 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _testFirebase() {
+    print("keep working you've got this!");
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference users = firestore.collection('users');
+    users.add({'name': 'First Last', 'age': 'turning 30', 'dob': 3});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,6 +95,10 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            TextButton(
+              child: const Text("Press Me to Test Firebase"),
+              onPressed: _testFirebase,
+            ),
             const Text(
               'You have pushed the button this many times:',
             ),
